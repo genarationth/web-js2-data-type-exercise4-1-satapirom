58b775a2860e9fde21b3d1a35767a91a9d714994
Satapirom Khawatthu. <Satapirom.12@gmail.com>
2024-05-13 16:05:21 +0700

    data type exercise 4-1

---
+++ script.js
@@ -0,0 +1,17 @@
+let products = {
+   product_id: 1,
+   product_name:"so good",
+   quantity_in_stock: 20,
+   unit_price: 120,
+   product_description:{
+    producdt_brand: "so good naka",
+    size_or_quantity:"1000 mL",
+    taste_or_scent:"sweet",
+    colour: "strawberry",
+    additional_description: "null"
+   }
+
+};
+
+console.log(products.unit_price);
+console.log(products.product_description.size_or_quantity);
